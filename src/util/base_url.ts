ea8819f811c0dd92ee1898a1f06c1beefeb8d2f2
ajeetpasi <sarojajeet660@gmail.com>
2025-05-10 19:05:21 +0530

    layout update

--- src/util/base_url.ts
+++ src/util/base_url.ts
@@ -1,1 +1,2 @@
-export const API_URL = "https://api.targetboardstore.com";
+// export const API_URL = "https://api.targetboardstore.com";
+export const API_URL = "https://starfish-app-qloib.ondigitalocean.app";
